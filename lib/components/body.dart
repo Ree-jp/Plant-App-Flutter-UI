a1ba1c11f44f74ab17060756d32587b2c42809ea
Ree_ <mail@ree-jp.net>
2020-12-28 01:02:30 +0900

    5day RecommendsPlants FeaturedPlants

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:plant_app/constants.dart';
 
+import 'featured_plants.dart';
 import 'header_with_searchbox.dart';
 import 'title_with_more_btn.dart';
+import 'recommend_plats.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -18,88 +20,16 @@ class Body extends StatelessWidget {
           title: "Recommended",
           press: () {},
         ),
-        Container(
-            margin: EdgeInsets.only(
-                left: kDefaultPadding,
-                top: kDefaultPadding / 2,
-                bottom: kDefaultPadding * 2.5),
-            width: size.width * 0.4,
-            child: Column(children: <Widget>[
-              Image.asset("assets/images/image_1.png"),
-              Container(
-                padding: EdgeInsets.all(kDefaultPadding),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 50,
-                        color: kPrimaryColor.withOpacity(0.23))
-                  ],
-                ),
-                child: Row(
-                  children: <Widget>[
-                    RichText(
-                      text: TextSpan(children: [
-                        TextSpan(
-                            text: "samantha\n".toUpperCase(), //全部大文字にする
-                            style: Theme.of(context).textTheme.button),
-                        TextSpan(
-                            text: "Russia".toUpperCase(),
-                            style: TextStyle(
-                                color: kPrimaryColor.withOpacity(0.5)))
-                      ]),
-                    ),
-                    Spacer(),
-                    Text(
-                      '\$440',
-                      style: Theme.of(context)
-                          .textTheme
-                          .button
-                          .copyWith(color: kPrimaryColor),
-                    )
-                  ],
-                ),
-              ),
-            ]))
+        RecommendsPlants(),
+        TitleWithMoreBtn(
+          title: "Featured Plants",
+          press: () {},
+        ),
+        FeaturedPlantCard(),
+        SizedBox(
+          height: kDefaultPadding,
+        )
       ],
     ));
-  }
-}
-
-class TitleWithCustomUnderline extends StatelessWidget {
-  const TitleWithCustomUnderline({
-    Key key,
-    this.text,
-  }) : super(key: key);
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 24, //文字の高さを制限する 収まらなかったら切れる
-      child: Stack(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
-            //文字をずらす
-            child: Text(text,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-          ),
-          Positioned(
-            //Stack内での絶対位置
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              margin: EdgeInsets.only(right: kDefaultPadding / 4), //線をずらす
-              height: 7, //高さを制限
-              color: kPrimaryColor.withOpacity(0.2), //色指定
-            ),
-          )
-        ],
-      ),
-    );
   }
 }
--- lib/components/title_with_more_btn.dart
+++ lib/components/title_with_more_btn.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:plant_app/constants.dart';
 
-import 'body.dart';
-
 class TitleWithMoreBtn extends StatelessWidget {
   const TitleWithMoreBtn({
     Key key,
@@ -31,6 +29,43 @@ class TitleWithMoreBtn extends StatelessWidget {
             child: Text(
               "more",
               style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithCustomUnderline extends StatelessWidget {
+  const TitleWithCustomUnderline({
+    Key key,
+    this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24, //文字の高さを制限する 収まらなかったら切れる
+      child: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+            //文字をずらす
+            child: Text(text,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          ),
+          Positioned(
+            //Stack内での絶対位置
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: EdgeInsets.only(right: kDefaultPadding / 4), //線をずらす
+              height: 7, //高さを制限
+              color: kPrimaryColor.withOpacity(0.2), //色指定
             ),
           )
         ],
