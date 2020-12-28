2b34bfc56b7c42b031aa4ddcea758fc89819c712
Ree_ <mail@ree-jp.net>
2020-12-28 17:34:08 +0900

    6day MyBottomNavBer

--- lib/screens/home/components/home_screen.dart
+++ lib/screens/home/components/home_screen.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plant_app/components/body.dart';
+import 'package:plant_app/components/my_bottom_nav_bar.dart';
 
 class HomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
       body: Body(),
+      bottomNavigationBar: MyBottomNavBar(),
     );
   }
 
