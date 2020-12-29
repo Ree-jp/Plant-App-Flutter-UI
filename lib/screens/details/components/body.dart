b93af8779edb1844a06aa16d459649832c38be92
Ree_ <mail@ree-jp.net>
2020-12-30 01:04:38 +0900

    7day datails img

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,17 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plant_app/constants.dart';
 
-class Body extends StatelessWidget{
+class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Column(
       children: <Widget>[
-        Row(
-          children: <Widget>[
-            Expanded(child: Column(
-
-            ))
-          ],
-        )
+        SizedBox(
+          height: size.height * 0.8,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                  child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: kDefaultPadding * 3),
+                child: Column(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                          icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          }),
+                    ),
+                    Spacer()
+                  ],
+                ),
+              )),
+              Container(
+                height: size.height * 0.8,
+                width: size.width * 0.75,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(63),
+                      bottomLeft: Radius.circular(63),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 60,
+                          color: kPrimaryColor.withOpacity(0.28)),
+                    ],
+                    image: DecorationImage(
+                        alignment: Alignment.centerLeft,
+                        fit: BoxFit.cover,
+                        image: AssetImage("assets/images/img.png"))),
+              )
+            ],
+          ),
+        ),
       ],
     );
   }
